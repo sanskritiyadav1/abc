4404730ef70d523f3af7f037fa2c1c4591e34000
Sanskriti Yadav <40791471+sanskritiyadav1@users.noreply.github.com>
2018-07-19 17:47:42 +0530

    Add files via upload

---
+++ edit.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class edit
+{
+	static int min(int x,int y,int z)
+	{
+		if(x<=y && x<=z)
+			return x;
+		if(y<=x && y<=z)
+			return y;
+		else
+			return z;
+	}
+	static int dist(String str1,String str2,int m,int n)
+	{
+		int dp[][]=new int[m+1][n+1];
+		for(int i=0;i<=m;i++)
+		{
+			for(int j=0;j<=n;j++)
+			{
+				if(i==0)
+					dp[i][j]=j;
+				else if(j==0)
+					dp[i][j]=i;
+				else if(str1.charAt(i-1)==str2.charAt(j-1))
+					dp[i][j]=dp[i-1][j-1];
+				else
+					dp[i][j]=1+min(dp[i][j-1],dp[i-1][j-1],dp[i-1][j]);
+			}
+		}
+		return dp[m][n];
+	}
+	public static void main(String[] args) {
+		String str1="geeks";
+		String str2="gks";
+		System.out.println(dist(str1,str2,str1.length(),str2.length()));
+	}
+}
